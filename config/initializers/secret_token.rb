b72f8cf42f4f40405ea62803667c201cb951dda7
Mrinal Kuchhal <mk@breakingwavesoftware.com>
2012-03-06 15:26:54 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RavenAjax::Application.config.secret_token = '2c064061a42d2ae9deaed43f514fe1b862d19744a2ec3cad5fda6a8b2574473530890ed6bb50ad022289d943829dedfc1958366fafd1a93d62b118d786242d2d'
